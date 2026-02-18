1b87bf2fc4b0a5acc612ab75431a49c57e5b8ef1
HeytorCardoso (Pessoal) <252641960+HeytorCardoso@users.noreply.github.com>
2026-02-17 21:19:46 -0300

    Final Version

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const group = document.querySelector('.group');
-        const clone = group.innerHTML;
-        group.innerHTML += clone;
-});
